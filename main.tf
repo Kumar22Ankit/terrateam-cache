1434046177ca7b162c62970cec713602ec084d68
Ankit Kumar <146810887+Kumar22Ankit@users.noreply.github.com>
2024-12-12 10:40:24 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,4 +1,4 @@
-resource "aws_instance" "cache_instance01" {
+resource "aws_instance" "cache_instances01" {
 ami = "ami-053b12d3152c0cc71"
 instance_type = "t2.nano"
 subnet_id = "0befd88457a8ccab3"
