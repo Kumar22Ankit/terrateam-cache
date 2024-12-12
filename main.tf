a2b4dc2f76090d2476f1aeda05df775d3ea053c3
Ankit Kumar <146810887+Kumar22Ankit@users.noreply.github.com>
2024-12-12 11:47:46 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 provider "aws" {
   region= "us-east-1" 
 }
-resource "aws_instance" "cache_instances01" {
+resource "aws_instance" "cache_instance01" {
 ami = "ami-0e2c8caa4b6378d8c"
 instance_type = "t2.micro"
 subnet_id = "subnet-03b09ae82ec9cd558"
